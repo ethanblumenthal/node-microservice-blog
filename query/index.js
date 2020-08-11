338443c5eb13e99b665c49ab4cf1eb261db41713
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-08-11 12:29:34 -0500

    Created kubernetes deployments for comments, moderation, and query services.

--- query/index.js
+++ query/index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const axios = require("axios");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,19 +10,19 @@ app.use(cors());
 const posts = {};
 
 const handleEvent = (type, data) => {
-  if (type === "PostCreated") {
+  if (type === 'PostCreated') {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
   }
 
-  if (type === "CommentCreated") {
+  if (type === 'CommentCreated') {
     const { id, content, postId, status } = data;
 
     const post = posts[postId];
     post.comments.push({ id, content, status });
   }
 
-  if (type === "CommentUpdated") {
+  if (type === 'CommentUpdated') {
     const { id, content, postId, status } = data;
 
     const post = posts[postId];
@@ -35,20 +35,20 @@ const handleEvent = (type, data) => {
   }
 };
 
-app.get("/posts", (req, res) => {
+app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post("/events", (req, res) => {
+app.post('/events', (req, res) => {
   const { type, data } = req.body;
   handleEvent(type, data);
   res.send({});
 });
 
 app.listen(4002, async () => {
-  console.log("Listening on 4002");
+  console.log('Listening on 4002');
 
-  const res = await axios.get("http://localhost:4005/events");
+  const res = await axios.get('http://event-bus-srv:4005/events');
   for (let event of res.data) {
     handleEvent(event.type, event.data);
   }
