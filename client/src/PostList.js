71b784100f63d091642933cc29c7b8e1bac0685d
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-08-11 13:07:39 -0500

    Added nginx ingress load balancer.

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import CommentCreate from "./CommentCreate";
-import CommentList from "./CommentList";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 export default () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const { data } = await axios.get("http://localhost:4002/posts");
+    const { data } = await axios.get('http://posts.com/posts');
     setPosts(data);
   };
 
@@ -18,11 +18,11 @@ export default () => {
   const renderedPosts = Object.values(posts).map((post) => {
     return (
       <div
-        className="card"
-        style={{ width: "30%", marginBottom: "20px" }}
+        className='card'
+        style={{ width: '30%', marginBottom: '20px' }}
         key={post.id}
       >
-        <div className="card-body">
+        <div className='card-body'>
           <h3>{post.title}</h3>
           <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
@@ -32,7 +32,7 @@ export default () => {
   });
 
   return (
-    <div className="d-flex flex-row flex-wrap justify-content-between">
+    <div className='d-flex flex-row flex-wrap justify-content-between'>
       {renderedPosts}
     </div>
   );
